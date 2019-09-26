0f895d1b901b89e657aae0db934f53bcd5f44ba9
Ubuntu <pkuittinen@ubuntupetrikuittinen.xbp5jv35e4rujjrlarqjj32eqf.fx.internal.cloudapp.net>
2019-09-26 12:42:54 +0000

    Latest version 26.9.2019

---
+++ lesson_geolocation/util.js
@@ -0,0 +1,8 @@
+const INCH_IN_CM = 2.54;
+function inchesToCm(inch) {
+    return inch*INCH_IN_CM;
+}
+function cmToInches(cm) {
+    return cm/INCH_IN_CM;
+}
+
---
+++ lesson_geolocation/util3.js
@@ -0,0 +1,8 @@
+const INCH_IN_CM = 2.54;
+export function inchesToCm(inch) {
+    return inch*INCH_IN_CM;
+}
+export function cmToInches(cm) {
+    return cm/INCH_IN_CM;
+}
+
