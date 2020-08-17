608284c3e239fdc657df0ab0ed5b7d6e070549b6
petrikuittinen <petri.kuittinen@hamk.fi>
2020-08-17 14:05:54 +0300

    poistin kaksi

--- lesson_python_basics/teht2.py
+++
@@ -1,9 +0,0 @@
-import re
-from statistics import mean, median
-s = input("Give me numbers?")
-number_strings = re.findall(r"-?\d+", s)
-numbers = list(map(int, number_strings))
-#numbers = [int(x) for x in number_strings]
-print("sum", sum(numbers))
-print("mean", mean(numbers))
-print("median", median(numbers))
