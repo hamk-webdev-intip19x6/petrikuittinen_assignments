f96d77a9945cb875e1ae41dcad3e7fc36961d298
petrikuittinen <petri.kuittinen@hamk.fi>
2020-10-01 12:42:14 +0300

    tiny changes again

--- lesson_javascript_modules/util3.js
+++ lesson_javascript_modules/util3.js
@@ -6,11 +6,3 @@ export function inchesToCm(inch) {
 export function cmToInches(cm) {
     return cm / INCH_IN_CM;
 }
-
-import { inchesToCm, cmToInches } from './util3.js';
-console.log(inchesToCm(12));
-import * as Util from './util3.js';
-console.log(Util.inchesToCm(12)); 
-console.log(Util.cmToInches(10));
-import { inchesToCm as inchConversion } from './util3.js';
-console.log(inchConversion(12));
