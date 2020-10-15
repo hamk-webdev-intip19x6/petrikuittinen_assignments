24e85be64b9fa289a462817728f77ea70df18dc9
petrikuittinen <petri.kuittinen@hamk.fi>
2020-10-15 15:10:12 +0300

    jquery

--- lesson_jquery/form_engine.js
+++ lesson_jquery/form_engine.js
@@ -1,35 +1,31 @@
 function answer(event) {
     let answers = $(":checked");
-    for(let answer of answers) {
-	let idMatch = answer.id.match(/question-(\d+)-(\d+)/);
-	let questionN = idMatch[1];
-	let choiceN = idMatch[2];
-	let points = questions[questionN].choices[choiceN].points;
-	console.log(answer.id, points);
+    for (let answer of answers) {
+        let idMatch = answer.id.match(/question-(\d+)-(\d+)/);
+        let questionN = idMatch[1];
+        let choiceN = idMatch[2];
+        let points = questions[questionN].choices[choiceN].points;
+        console.log(answer.id, points);
     }
     event.preventDefault();
     return false;
 }
 let form = "<form>\n";
-let questionN = 0;
-for(let question of questions) {
-    form += "<p>"+question.text+"<\/p>";
-    form += '<fieldset data-role="controlgroup" data-type="vertical">\n';
-    let choiceN = 0;
-    for(let choice of question.choices) {
-	form += '<input type="radio" id="question-'+
-	    questionN+'-'+choiceN+'"'+
-	    ' name="question-'+questionN+'" '+
-	    'value="'+choiceN+'">';
-	form += '<label for="question-'+questionN+'-'+choiceN+'">'+
-	    choice.text+'<\/label>\n';
-	choiceN++;
+for (let [questionN, question] of questions.entries()) {
+    form += `<p>${question.text}<\/p>`;
+    form += `<fieldset data-role="controlgroup" data-type="vertical">\n`;
+    for (let [choiceN, choice] of question.choices.entries()) {
+        form += `<input type="radio" id="question-` +
+            `${questionN}-${choiceN} name="question-${questionN}" ` +
+            `value="${choiceN}">`;
+        form += `<label for="question-${questionN}-${choiceN}">` +
+            `${choice.text}<\/label>\n`;
+        choiceN++;
     }
-    form += '</fieldset>\n';
+    form += `</fieldset>\n`;
     questionN++;
 }
-form += '<button class="ui-btn ui-shadow" onclick="answer(event)">'+
+form += '<button class="ui-btn ui-shadow" onclick="answer(event)">' +
     'Answer<\/button>';
 form += "<\/form>\n";
 document.getElementById("container").innerHTML = form;
-
